a816d55075e855b2413c0c4bfb07598d2ab338a5
Kenneth <rekerc4@gmail.com>
2018-08-02 11:28:54 -0400

    first js commit

--- script.js
+++ script.js
@@ -2,4 +2,14 @@
 
 $(document).ready(() => {
 
+let n = undefined; 
+
+
+$("body").on("click", ".login_button", (e) => {
+    console.log($(".login_form").children().eq(1).val()); 
+    console.log($(".login_form").children().eq(0).val()); 
+    &("#log_in_section").toggleClass(hidden); 
+})
+
+
 });
